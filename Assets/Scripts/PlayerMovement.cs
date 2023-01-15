adfe37a5e6c600bef685401becdb4ac0f7defaa3
Brett Torra <107492267+brettmt13@users.noreply.github.com>
2023-01-15 16:09:16 -0500

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Controls playerInput;
     public Rigidbody2D rb;
